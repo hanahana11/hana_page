6f1ac6f6cb00eba9726e1fb253d133234a15f4d0
hanahana11 <hana87kotaro@i.softbank.jp>
2020-12-19 14:55:22 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+$(function () {
+    setTimeout(function () {
+        $('.first-animation-img').each(function () {
+            $(this).css('opacity', '1');
+            $(this).css('transform', 'translateY(0)');
+        });
+        $('.first-animation-h1').each(function () {
+            $(this).css('opacity', '1');
+            $(this).css('transform', 'translateY(0)');
+        });
+        $('.first-animation-memo').each(function () {
+            $(this).css('opacity', '1');
+            $(this).css('transform', 'translateY(0)');
+        });
+    });
+
+    // scroll animation
+    $(window).scroll(function () {
+        $('.scroll-animation').each(function () {
+            let targetElement = $(this).offset().top;
+            let scroll = $(window).scrollTop();
+            let windowHeight = $(window).height();
+            if (scroll > targetElement - windowHeight) {
+                $(this).css('opacity', '1');
+                $(this).css('transform', 'translateY(0)');
+            }
+        });
+        
+//        $('.first-animation-img').each(function () {
+//            let targetElement = $(this).offset().top;
+//            let scroll = $(window).scrollTop();
+//            let windowHeight = $(window).height();
+//            if (scroll > targetElement - windowHeight/2) {
+//                $(this).css('opacity', '1');
+//                $(this).css('transform', 'translateY(0)');
+//            }
+//        });
+    });
+});
